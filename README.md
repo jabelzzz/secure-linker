b1fced67730f5700eda5b2f484a49ddd74aeaf03
Jabel <jabel.alvarez@zucchetti.com>
2025-07-30 15:30:31 +0100

    add: project first steps

---
+++ README.md
@@ -0,0 +1,6 @@
+Used technologies
+* Python
+* FastAPI
+* Secrets for the token generation
+* Uvicorn as WSGI
+* PostqreSQL to save the generated URLs
---
+++ src/secure_linker/static/js/index.js
@@ -0,0 +1,5 @@
+        // JavaScript básico para interactividad
+        document.querySelector('.button').addEventListener('click', function(e) {
+            e.preventDefault();
+            alert('¡Proyecto iniciado!');
+        });
