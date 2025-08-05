a1227bd66a9720dcbc1242ddc84cee65477e94da
Jabel <jabel.alvarez@zucchetti.com>
2025-08-05 17:09:30 +0100

    fix: some adjustements in the style

--- src/secure_linker/static/js/index.js
+++ src/secure_linker/static/js/index.js
@@ -1,5 +1,17 @@
-        // JavaScript básico para interactividad
-        document.querySelector('.button').addEventListener('click', function(e) {
-            e.preventDefault();
-            alert('¡Proyecto iniciado!');
-        });
+
+//Hide div when link is generated
+document.addEventListener("DOMContentLoaded", function() {
+    const resultContainer = document.querySelector('.result-container');
+    if (resultContainer) {
+        resultContainer.style.display = 'none';
+    }
+
+    // Show the result container when a link is generated
+
+    const form = document.querySelector('form');
+    form.addEventListener('submit', function(event) {
+        document.getElementById("form-container").hidden = true;
+        event.preventDefault(); // Prevent the default form submission
+        resultContainer.style.display = 'block'; // Show the result container
+    });
+});
